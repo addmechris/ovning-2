4601bf65070872d8cee51573e2ace335d57a10e9
addmechris <75683969+addmechris@users.noreply.github.com>
2021-12-06 17:17:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace övning_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 1;
+            do
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Navigate by typing a number from the list below:");
+                Console.WriteLine("\n");
+                Console.WriteLine("1: Check if age discount is applicable");
+                Console.WriteLine("2: Repeat ten times");
+                Console.WriteLine("3: The third word");
+                Console.WriteLine("4: Add tickets and calculate sum");
+                Console.WriteLine("0: Quit");
+                input = int.Parse(Console.ReadLine());
+                
+                switch (input)
+                {
+                    case 1:
+                        int age;
+                        Console.WriteLine("Type age");
+                        
+
+                        try
+                        {
+                            age = int.Parse(Console.ReadLine());
+
+                            if (age < 20)
+                            {
+                                Console.WriteLine("Discount for youth: 80 kr");
+                            }
+                            if(age > 64)
+                            {
+                                Console.WriteLine("Discount for elders: 90 kr");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No discount, standard price: 120 kr");
+                            }
+                        }
+                        catch (Exception e)
+                        {
+
+                            Console.WriteLine("Wrong input");
+                        }
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Type a sentence");
+                        var sentence = Console.ReadLine();
+                        int number = 1;
+                        for (int i = 0; i < 11;  i++)
+                        {
+                            
+                            Console.Write(number + "." + sentence + " ");
+                            number++;
+                        }
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Type a sentence consisting of at least three words");
+                        var phrase = Console.ReadLine();
+                        string[] words = phrase.Split(" ");
+                        Console.WriteLine("The third word is: " + words[2]);
+
+                        break;
+
+                    case 4:
+                        Console.WriteLine("How many tickets?");
+                        int arraysize = int.Parse(Console.ReadLine());
+
+                        int[] tickets = new int[arraysize];
+
+                        /*
+                        foreach (var item in tickets)
+                        {
+                            Console.WriteLine("Age?");
+                            int age_ticket = int.Parse(Console.ReadLine());
+
+                            if (age_ticket < 20)
+                            {
+                                tickets[arraysize] = 80;
+
+                            }
+                            if (age_ticket > 64)
+                            {
+                                tickets[arraysize] = 90;
+                            }
+                            else
+                            {
+                                tickets[arraysize] = 120;
+                            }
+                            
+                        }
+                        */
+
+                        for (int i = arraysize; i <= arraysize; i--)                      
+                        {
+                            Console.WriteLine("Age?");
+                           int age_ticket = int.Parse(Console.ReadLine());
+
+                            if (age_ticket < 20)
+                            {
+                                tickets[i] = 80; 
+                                
+                            }
+                            if (age_ticket > 64)
+                            {
+                                tickets[i] = 90;
+                            }
+                            else
+                            {
+                                tickets[i] = 120;
+                            }
+                        }
+                        
+
+                        int sum = 0;
+                        foreach (var item in tickets)
+                        {
+                            sum += item;
+                            
+                        }
+                        Console.WriteLine("The total sum is: " + sum);
+                        break;
+                    case 0:
+                        
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong input");
+                        break;
+                }
+
+            } while (input != 0);
+
+        }
+
+       
+
+
+    }
+}
